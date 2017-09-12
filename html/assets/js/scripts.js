697e344e3c077932a7ae5eb6e33e112744b17bd5
harold buenafe <rold@corporatevaluelimited.net>
2017-09-12 17:01:46 +0800

    HTML Updates

--- html/assets/js/scripts.js
+++ html/assets/js/scripts.js
@@ -1,36 +1,42 @@
  $(document).ready(function() {
-        // Initializes search overlay plugin.
-        // Replace onSearchSubmit() and onKeyEnter() with 
-        // your logic to perform a search and display results
-        $('[data-pages="search"]').search({
-            searchField: '#overlay-search',
-            closeButton: '.overlay-close',
-            suggestions: '#overlay-suggestions',
-            brand: '.brand',
-            onSearchSubmit: function(searchString) {
-                console.log("Search for: " + searchString);
-            },
-            onKeyEnter: function(searchString) {
-                console.log("Live search for: " + searchString);
-                var searchField = $('#overlay-search');
-                var searchResults = $('.search-results');
-                clearTimeout($.data(this, 'timer'));
-                searchResults.fadeOut("fast");
-                var wait = setTimeout(function() {
-                    searchResults.find('.result-name').each(function() {
-                        if (searchField.val().length != 0) {
-                            $(this).html(searchField.val());
-                            searchResults.fadeIn("fast");
-                        }
-                    });
-                }, 500);
-                $(this).data('timer', wait);
-            }
-        });
+    // Initializes search overlay plugin.
+    // Replace onSearchSubmit() and onKeyEnter() with 
+    // your logic to perform a search and display results
+    $('[data-pages="search"]').search({
+        searchField: '#overlay-search',
+        closeButton: '.overlay-close',
+        suggestions: '#overlay-suggestions',
+        brand: '.brand',
+        onSearchSubmit: function(searchString) {
+            console.log("Search for: " + searchString);
+        },
+        onKeyEnter: function(searchString) {
+            console.log("Live search for: " + searchString);
+            var searchField = $('#overlay-search');
+            var searchResults = $('.search-results');
+            clearTimeout($.data(this, 'timer'));
+            searchResults.fadeOut("fast");
+            var wait = setTimeout(function() {
+                searchResults.find('.result-name').each(function() {
+                    if (searchField.val().length != 0) {
+                        $(this).html(searchField.val());
+                        searchResults.fadeIn("fast");
+                    }
+                });
+            }, 500);
+            $(this).data('timer', wait);
+        }
+    });
+
+    $('#btnStickUp').click(function() {
+        $('#myModal').modal('show');
+    });
      
-        $('#btnStickUp').click(function() {
-            var modalElem = $('#myModal');
-            
-            modalElem.modal('show');
-        });
-    })
+    $('#btnNewLocation').click(function() {
+        $('.modal-new-location').modal('show');
+    });
+     
+    $('.btnEditLocation').click(function() {
+        $('.modal-edit-location').modal('show');
+    });
+})
